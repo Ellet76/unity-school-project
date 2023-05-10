4df7824cb962c7cfe514905592b0d8eb90185db1
Ellet76 <122363941+Ellet76@users.noreply.github.com>
2023-05-10 22:18:44 +0200

    new stuff

---
+++ Assets/Scripts/ObjectEditManager.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class ObjectEditManager : MonoBehaviour
+{
+    public Tilemap floor;
+    private GridLayout floorMap;
+
+    public TMP_Dropdown machineSelection;
+
+
+    void Start()
+    {
+        floorMap = floor.GetComponent<GridLayout>();
+    }
+
+    void Update()
+    {
+        
+        if (machineSelection.options[machineSelection.value].text ==  "Edit")
+        {
+            if (Input.GetMouseButtonDown(0) == true)
+            {
+                Debug.Log("Recognized Button press");
+                DeleteMachine();
+            }
+            if (Input.GetKeyDown(KeyCode.R) == true)
+            {
+
+            }
+        }
+    }
+
+    void DeleteMachine()
+    {
+        Vector3Int gridMousePosition = ConvertMousePos();
+        RaycastHit2D hit = Physics2D.Raycast(new Vector2(gridMousePosition.x + 1, gridMousePosition.y + 1), Vector2.up, 0f);
+        if (hit.collider != null)
+        {
+            Destroy(hit.collider.gameObject);
+        }
+    }
+
+    void Rotation()
+    {
+        Vector3Int gridMousePosition = ConvertMousePos();
+        RaycastHit2D hit = Physics2D.Raycast(new Vector2(gridMousePosition.x + 1, gridMousePosition.y + 1), Vector2.up, 0f);
+        if (hit.collider != null)
+        {
+            GameObject machine = hit.collider.gameObject;
+            Quaternion pastRotation = machine.transform.rotation;
+            Vector3 newAngle = pastRotation.eulerAngles;
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                newAngle.z += 90;
+            }
+            else if(Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                newAngle.z -= 90;
+            }
+            machine.transform.rotation = Quaternion.Euler(newAngle);
+        }
+    }
+
+    Vector3Int ConvertMousePos()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3Int gridPosition = floorMap.WorldToCell(mousePosition);
+        return gridPosition;
+    }
+}
--- Assets/Scripts/ObjectPlaceManager.cs
+++ Assets/Scripts/ObjectPlaceManager.cs
@@ -1,23 +1,65 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class ObjectPlaceManager : MonoBehaviour
 {
     public List<GameObject> machines;
+    public Transform parent;
+
+    public Tilemap floor;
+    private GridLayout floorMap;
 
     public TMP_Dropdown machineSelection;
 
 
     void Start()
     {
-        
+        floorMap = floor.GetComponent<GridLayout>();
     }
 
     
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            try
+            {
+                PlaceObject(machines[machineSelection.value - 1]);
+            }
+            catch (System.Exception) 
+            { }
+        }    
+    }
+
+
+    Vector3Int ConvertMousePos()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3Int gridPosition = floorMap.WorldToCell(mousePosition);
+        gridPosition.x = gridPosition.x + 1;
+        gridPosition.y = gridPosition.y + 1;
+        return gridPosition;
+    }
+
+    void PlaceObject(GameObject machine)
+    {
+        Vector3Int gridMousePosition = ConvertMousePos();
+        Vector3Int realMousePos = floorMap.WorldToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+
+
+        if (floor.HasTile(realMousePos))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(new Vector2(gridMousePosition.x, gridMousePosition.y), Vector2.up, 0f);
+            if (hit.collider == null)
+            {
+                GameObject machineObject = Instantiate(machine, gridMousePosition, Quaternion.identity);
+                machineObject.transform.SetParent(parent);
+            }
+        }
     }
 }
